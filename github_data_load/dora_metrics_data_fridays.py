b810d186c050f810442ce4d138b064184ba6bf35
bridge-demo <139315052+bridge-demo@users.noreply.github.com>
2023-11-15 10:55:58 -0600

    Update dora_metrics_data_fridays.py
    add data for release management

--- github_data_load/dora_metrics_data_fridays.py
+++ github_data_load/dora_metrics_data_fridays.py
@@ -16,11 +16,28 @@ def parser( ) -> dict:
 def main():
     setup =  parser()
     repoConnection = Github( setup["gitHubToken"] ).get_repo( setup["repo"] )
-    releaseNumber = time.strftime("%Y.%m.%d") #Using number of the week as release counter
+    releaseNumber = time.strftime("%Y.%m.%d") 
     releaseBranchName = f"release-{releaseNumber}"
     create_branch( repoConnection=repoConnection, newBranchName=releaseBranchName )
+    
+    ## Release management data
+    print(f"Creating sev1 for {releaseBranchName}")
+    labelsForSev1 = [f"found_{releaseBranchName}", "sev1", "bug", ]
+    sev1_issueNumber = create_issue(repoConnection=repoConnection, labels=labelsForSev1, title=f"Severity 1 issue - release {releaseBranchName}" )
+    print(f"issue number -> {sev1_issueNumber}")
+    
+    print(f"Creating sev2 for {releaseBranchName}")
+    labelsForSev2 = [f"found_{releaseBranchName}", "sev2", "bug", "User Story" ]
+    sev2_issueNumber = create_issue( repoConnection=repoConnection, labels=labelsForSev2, title=f"Severity 2 issue - release {releaseBranchName}" )
+    print(f"issue number -> {sev2_issueNumber}")
+
+    ## /Release management data
+
     close_issues(repoConnection=repoConnection, Labelfilters=["User Story", "Epic"])
+    # Not closing the sev issues
+    # close_issues(repoConnection=repoConnection, Labelfilters=labelsForSev1)
+    # close_issues(repoConnection=repoConnection, Labelfilters=labelsForSev2)
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     main()
